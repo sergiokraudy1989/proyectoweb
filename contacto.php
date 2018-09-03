095d9f40cfc465c752176248185099a26b3c581c
sergiokraudy1989 <parnasokraudy@yahoo.com>
2018-09-03 14:05:41 -0600

    Este es el cuarto commit

--- contacto.php
+++ contacto.php
@@ -2,9 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>este es el contacto</title>
+	<title>Contacto</title>
 </head>
 <body>
-	
+	   <h1>Hola mundo con codigo facilito</h1>
+	   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus alias et, aliquid enim, mollitia ex voluptates ipsa doloribus temporibus illo itaque laudantium beatae repudiandae iste facilis delectus, est pariatur recusandae?</p>
 </body>
 </html>
