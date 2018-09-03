d253c82e8809052cfaf6faa03f29cdfe35e8fa72
sergiokraudy1989 <parnasokraudy@yahoo.com>
2018-09-03 11:27:04 -0600

    segundo commit

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Principal</title>
 </head>
 <body>
-	
+      <p>segundo commig</p>
+      <h2>prueba</h2>
+
+
+
+      <p>jgfgjhgdhbh</p>
+      	<p></p>
+      	<p></p>	
 </body>
 </html>
